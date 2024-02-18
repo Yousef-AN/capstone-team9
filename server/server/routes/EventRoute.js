c195ac6e69613152ed2fc7924f88f97988b18ec4
sarajouma <144836947+sarajouma@users.noreply.github.com>
2024-02-18 23:43:53 +0300

    fix api route issue

--- server/server/routes/EventRoute.js
+++ server/server/routes/EventRoute.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Event = require('../controllers/Event');
 
-router.post("/", Event.addNewEvent);
+router.post("/addNewEvent", Event.addNewEvent);
 router.put("/:id", Event.updateEvent);
-router.get("/:id",Event.getEventDetials);
-router.get('/', Event.getEvents);
-router.get('/', Event.getPopularEvent);
-router.get('/', Event.getPopularEvent);
-router.get('/', Event.getSliderEvent);
+router.get("/getEventDetials/:id",Event.getEventDetials);
+router.get('/getEvents', Event.getEvents);
+router.get('/getPopularEvent', Event.getPopularEvent);
+router.get('/getFeaturedEvent', Event.getFeaturedEvent);
+router.get('/getSliderEvent', Event.getSliderEvent);
 
 
 module.exports = router;
