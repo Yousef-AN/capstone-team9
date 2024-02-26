7839ccabb66dc1ce23165a97988cce2f1d9c606b
Yousef. <kingyousef922@gmail.com>
2024-02-26 21:30:55 +0300

    final edit =)

--- server/server/routes/TicketRoute.js
+++ server/server/routes/TicketRoute.js
@@ -5,13 +5,15 @@ const ticketController = require('../controllers/ticket');
 const router = express.Router();
 
 // This route is for handling GET requests to retrieve details of a specific ticket by its ID
-router.get('/:GetticketId', ticketController.getTicketById);
+router.get('/:GetTicketId', ticketController.getTicketById);
 
 // This route is for handling PUT requests to update details of a specific ticket by its ID
-router.put('/:UpdateticketId', ticketController.updateTicketById);
+router.put('/:UpdateTicketId', ticketController.updateTicketById);
 
 // This route is for handling DELETE requests to delete a specific ticket by its ID
-router.delete('/:DeleteticketId', ticketController.deleteTicketById);
+router.delete('/:DeleteTicketId', ticketController.deleteTicketById);
 
+// This route is for handling POST requests to add a new ticket
+router.post('/CreateTicket', ticketController.createTicket); // Add a new ticket
 
 module.exports = router;
